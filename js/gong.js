a6b5fdc95dea879afc77c395632ef2a7361aa046
Stanislav Shapkin <shapkin359@gmail.com>
2018-12-24 23:07:05 +0300

    исправлен русский язык

--- js/gong.js
+++ js/gong.js
@@ -9,33 +9,35 @@ button.addEventListener('click', timerRun);
 
 function timerRun(event) {
   event.stopPropagation();
-
-  time = 71;
+  audio.src = './sounds/beep.mp3';
+  time = 23;
   button.removeEventListener('click', timerRun);
   timer = setInterval(tick, 1000); 
 }
 
 function tick() {
   if(time > 0) {
-    time--;
     beep();
+    time--;
     if(time > 10) {
-      timerScreen.textContent = 'Время: ' + (time - 10) + ' секунд';
+      timerScreen.textContent = 'Время на обсуждение: ' + (time - 10) + ' секунд';
+      addLetter();
     } else {
       timerScreen.textContent = 'Время вышло. Сдаем ответы: ' + time + ' секунд';
+      addLetter();
     }
   } else if (time === 0) {
     audio.src = './sounds/beep-final.mp3';
     audio.play();
     clearInterval(timer);
     timerScreen.textContent = 'Время';
-    audio.src = './sounds/beep.mp3';
     button.addEventListener('click', timerRun);
   }
 }
 
 function beep() {
   switch (time) {
+    case 71:
     case 21:
     case 11:
     case 3:
@@ -43,5 +45,13 @@ function beep() {
     case 1:
       audio.play();
       break;
+  }
+}
+
+function addLetter() {
+  if (((time % 10 === 2) || (time % 10 === 3) || (time % 10 === 4)) && ((time !== 22) && (time !== 23) && (time !== 24))) {
+    timerScreen.textContent += 'ы';
+  } else if ((time % 10 === 1) && (time !== 21)) {
+    timerScreen.textContent += 'а';
   }
 }
